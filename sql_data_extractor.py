5ed97f6a7c878f92328b1c384482b2dc7c3bd5a5
Bastian Schindler <bastian.schindler@informatik.tu-chemnitz.de>
2025-02-26 23:27:37 +0100

    Try format Data

--- sql_data_extractor.py
+++ sql_data_extractor.py
@@ -1,11 +1,15 @@
 import mysql.connector
+import mysql.connector.errorcode as errorcode
 import pandas as pd
 from dotenv import load_dotenv
 import os
 from datetime import datetime
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler, RobustScaler
+from typing import Optional
 
 # Load database credentials from .env
-load_dotenv()
+load_dotenv(override=True)
 
 inputstring = input("Database_name (int or japan (Default)): ").lower() or 'japan'
 
@@ -48,16 +52,18 @@ def process_year_data(start_date = "1970-08-01", end_date= "1971-02-28", target_
     diff_full = (bloom_data['bloom_date'].values[0] - datetime.strptime(end_date, "%Y-%m-%d").date()).days - 1 # Subtract 1 to ignore the 28th of February
     diff_first = (bloom_data['first_bloom_date'].values[0] - datetime.strptime(end_date, "%Y-%m-%d").date()).days - 1 # Subtract 1 to ignore the 28th of February
 
-    days_to_full = (bloom_data['bloom_date'].values[0] - datetime.strptime(end_date, "%Y-%m-%d").date()).days
+    days_to_full = (bloom_data['bloom_date'].values[0] - datetime.strptime(start_date, "%Y-%m-%d").date()).days
     days_to_first = (bloom_data['first_bloom_date'].values[0] - datetime.strptime(start_date, "%Y-%m-%d").date()).days
 
     bloom_offset = (bloom_data['bloom_date'].values[0] - bloom_data['first_bloom_date'].values[0]).days
 
-    print("Days to Full Bloom: ", days_to_full, " Days to First Bloom: ", days_to_first, " Bloom Offset: ", bloom_offset)
-    return
+    # print("Days to Full Bloom: ", days_to_full, " Days to First Bloom: ", days_to_first, " Bloom Offset: ", bloom_offset)
 
     countdown_to_first = list(range(days_to_first, -bloom_offset - 1, -1))  # [days_to_first, ..., -bloom_offset]
     countdown_to_full = list(range(days_to_full, -1, -1))
+
+    # print("Countdown to Full Bloom: ", countdown_to_full, " Countdown to First Bloom: ", countdown_to_first)
+
     # print("Difference in days: ", diff_full, " First Bloom Date: ", diff_first)
 
     if diff_full == None:
@@ -87,16 +93,20 @@ def process_year_data(start_date = "1970-08-01", end_date= "1971-02-28", target_
 
     # Create the input dataframe
     input_data = {
-        "temperature_array": [temperature_array],
-        "latitude": [latitude],
-        "longitude": [longitude],
-        "altitude": [altitude]
+        "temperature_array": np.array(temperature_array),
+        "latitude": [latitude] * len(temperature_array),
+        "longitude": [longitude] * len(temperature_array),
+        "altitude": [altitude] * len(temperature_array)
     }
     input_df = pd.DataFrame(input_data)
 
+    # output_data = {
+    #     "bloom_days": [diff_full],
+    #     "first_bloom_days": [diff_first]
+    # }
     output_data = {
-        "bloom_days": [diff_full],
-        "first_bloom_days": [diff_first]
+        "bloom_day_countdown": np.array(countdown_to_full),
+        "first_bloom_day_countdown": np.array(countdown_to_first)
     }
     output_df = pd.DataFrame(output_data)
     
@@ -112,13 +122,221 @@ def process_year_data(start_date = "1970-08-01", end_date= "1971-02-28", target_
     input_df.to_csv(input_file, index=False)
     output_df.to_csv(output_file, index=False)
 
+    if not os.path.exists("tmp_full.txt"):
+        with open("tmp_full.txt", 'w') as file:
+            for number in countdown_to_full:
+                file.write(str(number) + " ")
+            file.write("\n")
+    else:
+        with open("tmp_full.txt", 'a') as file:
+            for number in countdown_to_full:
+                file.write(str(number) + " ")
+            file.write("\n")
+
+    if not os.path.exists("tmp_first.txt"):
+        with open("tmp_first.txt", 'w') as file:
+            for number in countdown_to_first:
+                file.write(str(number) + " ")
+            file.write("\n")
+    else:
+        with open("tmp_first.txt", 'a') as file:
+            for number in countdown_to_first:
+                file.write(str(number) + " ")
+            file.write("\n")
+
+    if not os.path.exists("tmp_temp.txt"):
+        with open("tmp_temp.txt", 'w') as file:
+            for number in temperature_array:
+                file.write(str(number) + " ")
+            file.write("\n")
+    else:
+        with open("tmp_temp.txt", 'a') as file:
+            for number in temperature_array:
+                file.write(str(number) + " ")
+            file.write("\n")
+
+    if not os.path.exists("tmp_lat.txt"):
+        with open("tmp_lat.txt", 'w') as file:
+            file.write(str(latitude) + "\n")
+    else:
+        with open("tmp_lat.txt", 'a') as file:
+            file.write(str(latitude) + "\n")
+
+    if not os.path.exists("tmp_lng.txt"):
+        with open("tmp_lng.txt", 'w') as file:
+            file.write(str(longitude) + "\n")
+    else:
+        with open("tmp_lng.txt", 'a') as file:
+            file.write(str(longitude) + "\n")
+    
+    if not os.path.exists("tmp_alt.txt"):
+        with open("tmp_alt.txt", 'w') as file:
+            file.write(str(altitude) + "\n")
+    else:
+        with open("tmp_alt.txt", 'a') as file:
+            file.write(str(altitude) + "\n")
+
     # print(f"Input dataset saved as {input_file}")
     # print(f"Output dataset saved as {output_file}")
+
+def scale_data(script_dir):
+    if inputstring == 'int':
+        folder_path_input = os.path.join(script_dir, "data/sql/int/input")
+        folder_path_output = os.path.join(script_dir, "data/sql/int/output")
+    elif inputstring == 'japan':
+        folder_path_input = os.path.join(script_dir, "data/sql/japan/input")
+        folder_path_output = os.path.join(script_dir, "data/sql/japan/output")
+
+    with open("tmp_full.txt", 'r') as file:
+        countdown_to_full_lines = file.readlines()
+    with open("tmp_first.txt", 'r') as file:
+        countdown_to_first_lines = file.readlines()
+    with open("tmp_temp.txt", 'r') as file:
+        temp_lines = file.readlines()
+    with open("tmp_lat.txt", 'r') as file:
+        lat_lines = file.readlines()
+    with open("tmp_lng.txt", 'r') as file:
+        lng_lines = file.readlines()
+    with open("tmp_alt.txt", 'r') as file:
+        alt_lines = file.readlines()
+
+    int_full_lines = [line.strip().split(" ") for line in countdown_to_full_lines]
+    int_first_lines = [line.strip().split(" ") for line in countdown_to_first_lines]
+    temp_lines = [line.strip().split(" ") for line in temp_lines]
+    lat_lines = [line.strip().split(" ") for line in lat_lines]
+    lng_lines = [line.strip().split(" ") for line in lng_lines]
+    alt_lines = [line.strip().split(" ") for line in alt_lines]
+    # print(temp_lines[0])
+
+    for element in int_full_lines:
+        element = [int(x) for x in element]
+    for element in int_first_lines:
+        element = [int(x) for x in element]
+    for element in temp_lines:
+        if len(element) <= 1:
+            continue
+        element = [float(x) for x in element]
+    for element in lat_lines:
+        element = [float(x) for x in element]
+    for element in lng_lines:
+        element = [float(x) for x in element]
+    for element in alt_lines:
+        element = [float(x) for x in element]
+
+    all_values_first = np.concatenate([np.array(x).flatten() for x in int_full_lines if len(x) > 0])
+    all_values_full = np.concatenate([np.array(x).flatten() for x in int_first_lines if len(x) > 0])
+    all_values_temp = np.concatenate([np.array(x).flatten() for x in temp_lines if len(x) > 1])
+    all_values_lat = np.concatenate([np.array(x).flatten() for x in lat_lines if len(x) > 0])
+    all_values_lng = np.concatenate([np.array(x).flatten() for x in lng_lines if len(x) > 0])
+    all_values_alt = np.concatenate([np.array(x).flatten() for x in alt_lines if len(x) > 0])
+
+    scaler_kwargs: Optional[dict] = None
+    scaler_kwargs = scaler_kwargs or {}
+    scaler_kwargs['feature_range'] = (-1, 1)
+    scaler_first = MinMaxScaler(**scaler_kwargs)
+    scaler_full = MinMaxScaler(**scaler_kwargs)
+    scaler_temp = MinMaxScaler(**scaler_kwargs)
+
+    scaler_kwargs: Optional[dict] = {'quantile_range': (5, 95)}
+    
+    robust_lat = RobustScaler(**scaler_kwargs)
+    robust_lng = RobustScaler(**scaler_kwargs)
+    robust_alt = RobustScaler(**scaler_kwargs)
+
+    # Fit scaler on all values
+    scaler_first.fit(all_values_first.reshape(-1, 1))
+    scaler_full.fit(all_values_full.reshape(-1, 1))
+    scaler_temp.fit(all_values_temp.reshape(-1, 1))
+
+    robust_lat.fit(all_values_lat.reshape(-1, 1))
+    robust_lng.fit(all_values_lng.reshape(-1, 1))
+    robust_alt.fit(all_values_alt.reshape(-1, 1))
+
+        # Function to scale lists/arrays
+    def scale_list_first(x):
+        if isinstance(x, int): 
+            x = [x]
+        if len(x) == 0:
+            return x
+        return scaler_first.transform(np.array(x).reshape(-1, 1)).flatten().tolist()
+    
+    def scale_list_full(x):
+        if isinstance(x, int):  
+            x = [x]
+        if len(x) == 0:
+            return x
+        return scaler_full.transform(np.array(x).reshape(-1, 1)).flatten().tolist()
+    
+    def scale_list_temp(x):
+        if isinstance(x, float): 
+            x = [x]
+        if len(x) == 0:
+            return x
+        return scaler_temp.transform(np.array(x).reshape(-1, 1)).flatten().tolist()
+    
+    def scale_list_lat(x):
+        if isinstance(x, float): 
+            x = [x]
+        if len(x) == 0:
+            return x
+        return robust_lat.transform(np.array(x).reshape(-1, 1)).flatten().tolist()
+    
+    def scale_list_lng(x):
+        if isinstance(x, float): 
+            x = [x]
+        if len(x) == 0:
+            return x
+        return robust_lng.transform(np.array(x).reshape(-1, 1)).flatten().tolist()
+    
+    def scale_list_alt(x):
+        if isinstance(x, float): 
+            x = [x]
+        if len(x) == 0:
+            return x
+        return robust_alt.transform(np.array(x).reshape(-1, 1)).flatten().tolist()
+
+    # Get all files and folders in the directory
+    files_and_folders = os.listdir(folder_path_output)
+
+    # Filter out only files
+    files = [file for file in files_and_folders if os.path.isfile(os.path.join(folder_path_output, file))]
+
+    for file in files:
+        # Read the file
+        df = pd.read_csv(os.path.join(folder_path_output, file))
+        # print(df["bloom_day_countdown"])
+        # print(df["first_bloom_day_countdown"])
+
+        # Apply scaling to each list in the column
+        df['bloom_day_countdown'] = df['bloom_day_countdown'].apply(scale_list_full)
+        df['first_bloom_day_countdown'] = df['first_bloom_day_countdown'].apply(scale_list_first)
+
+        # Save the file
+        df.to_csv(os.path.join(folder_path_output, file), index=False)
+
+    files_and_folders = os.listdir(folder_path_input)
+    files = [file for file in files_and_folders if os.path.isfile(os.path.join(folder_path_input, file))]
+
+    for file in files:
+        # Read the file
+        df = pd.read_csv(os.path.join(folder_path_input, file))
+
+        # Apply scaling to each list in the column
+        df["temperature_array"] = df['temperature_array'].apply(scale_list_temp)
+        df["latitude"] = df['latitude'].apply(scale_list_lat)
+        df["longitude"] = df['longitude'].apply(scale_list_lng)
+        df["altitude"] = df['altitude'].apply(scale_list_alt)
+
+        # Save the file
+        df.to_csv(os.path.join(folder_path_input, file), index=False)
+
+    # Apply scaling to each list in the column
+    # df[column_name] = df[column_name].apply(scale_list)
 
 def international_data_loader():
     start_date = "08-01"
     end_date = "02-28"
-    target_station_international = ["Liestal_Weideli", "New_York", "Vancouver", "Washington"]
+    target_station_international = ["Washington", "Liestal_Weideli", "New_York", "Vancouver"]
     for station in target_station_international:
         station_bloom = station + "_bloom"
 
@@ -139,9 +357,18 @@ def international_data_loader():
         # print("Lowest Date: ", lowest_date)
 
         for year in years:
-            if year not in years_check and year - 1 not in years_check:
+            if year not in years_check or year - 1 not in years_check:
                 continue
-            process_year_data(str(year-1)+"-"+start_date, str(year)+"-"+end_date, station, station_bloom)   
+            process_year_data(str(year-1)+"-"+start_date, str(year)+"-"+end_date, station, station_bloom)  
+
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    scale_data(script_dir)
+    os.remove(os.path.join(script_dir, "tmp_full.txt"))
+    os.remove(os.path.join(script_dir, "tmp_first.txt"))
+    os.remove(os.path.join(script_dir, "tmp_temp.txt"))
+    os.remove(os.path.join(script_dir, "tmp_lat.txt"))
+    os.remove(os.path.join(script_dir, "tmp_lng.txt"))
+    os.remove(os.path.join(script_dir, "tmp_alt.txt"))
 
 def japan_data_loader():
     start_date = "08-01"
